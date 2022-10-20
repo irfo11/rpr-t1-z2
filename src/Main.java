b8d22f4a08c2d3bb8dd3db42a2790ffebd89b1af
irfo1 <irfosabanovic@gmail.com>
2022-10-20 17:16:13 +0200

    prvi commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    static int sumaCifara(int n){
+        int suma = 0;
+        while(n != 0){
+            suma += n % 10;
+            n /= 10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Unesite n -> ");
+        Scanner ulaz = new Scanner(System.in);
+        int n = ulaz.nextInt();
+        for(int i=2; i<n; i++){
+            if(i % sumaCifara(i) == 0)
+                System.out.println(i);
+        }
+    }
+}
